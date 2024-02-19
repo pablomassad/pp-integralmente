ff04d404c63c753bf49c0898b9991724e102f332
Pablo Massad <pamassad@teco.com.ar>
2024-02-19 09:04:07 -0300

    cfunctions

--- functions/index.js
+++ functions/index.js
@@ -1,55 +1,59 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
-const execSync = require('child_process').execSync;
-const cors = require('cors')({origin: true});
-const moment = require('moment');
+const nodemailer = require('nodemailer')
+const cors = require('cors')({ origin: true })
+const moment = require('moment')
+const smtpTransport = require('nodemailer-smtp-transport')
+const execSync = require('child_process').execSync
+
+// const { Vonage } = require('@vonage/server-sdk')
+// const vonage = new Vonage({
+//     apiKey: "4ef27424",
+//     apiSecret: "bhZpr92ly4tAfCWf"
+// })
 
 
 admin.initializeApp(functions.config().firebase)
 const afs = admin.firestore()
 
 exports.reportPos = functions.https.onCall((data, context) => {
-    const lat = data.lat;
-    const lng = data.lng;
+    const lat = data.lat
+    const lng = data.lng
 
     const pos = 'pos: ' + lat + ';' + lng
     console.log(pos)
-    sendPos(lat,lng)
-    return pos;
+    sendPos(lat, lng)
+    return pos
 })
 exports.reportGPS = functions.https.onRequest((request, response) => {
-    response.set('Access-Control-Allow-Origin', '*');
-    response.set('Access-Control-Allow-Methods', 'GET, POST');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
+    response.set('Access-Control-Allow-Origin', '*')
+    response.set('Access-Control-Allow-Methods', 'GET, POST')
+    response.set('Access-Control-Allow-Headers', 'Content-Type')
 
-    const lat = request.query.lat;
-    const lng = request.query.lng;
+    const lat = request.query.lat
+    const lng = request.query.lng
 
     const pos = 'pos: ' + lat + ';' + lng
     console.log(pos)
-    sendPos(lat,lng)
-    response.send("Se envio " + pos);
+    sendPos(lat, lng)
+    response.send("Se envio " + pos)
 })
 exports.command = functions.https.onRequest((request, response) => {
-    response.set('Access-Control-Allow-Origin', '*');
-    response.set('Access-Control-Allow-Methods', 'GET, POST');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
+    response.set('Access-Control-Allow-Origin', '*')
+    response.set('Access-Control-Allow-Methods', 'GET, POST')
+    response.set('Access-Control-Allow-Headers', 'Content-Type')
 
-    const topic = request.query.topic;
-    const cmd = request.query.cmd;
-    const args = request.query.args;
+    const topic = request.query.topic
+    const cmd = request.query.cmd
+    const args = request.query.args
     sendCommand(topic, cmd, args)
-    response.send("Msg: topic:"+ topic + " cmd:" + cmd + ' args:' + args);
+    response.send("Msg: topic:" + topic + " cmd:" + cmd + ' args:' + args)
 })
-exports.happyBirthday = functions.https.onRequest((request, response) =>
-{
+exports.happyBirthday = functions.https.onRequest((request, response) => {
     fcmPush('YBHqrkv2VBS5VAJuWweey1TO8zf2', 'Pablito')
-    response.send("Happy Birtday!");
+    response.send("Happy Birtday!")
 })
-exports.evalCumples = functions.https.onRequest(async (request, response) =>
-{
+exports.evalCumples = functions.https.onRequest(async (request, response) => {
     const today = moment().format('MMDD')
     let birthdaysErrors = 0
     let birthdaysToday = 0
@@ -117,18 +121,55 @@ exports.evalCumples = functions.https.onRequest(async (request, response) =>
     console.log(rta)
     response.send('total: ' + rta + ' ==> fecha:' + today + ' cumples hoy: ' + birthdaysToday + ' errors: ' + birthdaysErrors)
 })
-exports.sendMail = functions.https.onRequest(async (req, res) =>
-{
+exports.sendMail = functions.https.onRequest(async (req, res) => {
     const mail = req.body.mail // req.query.mail;
     const person = req.body.person //req.query.person;
 
     const rta = await sendMailTo(mail, person)
     res.send(rta)
 })
+exports.sendSMSFCM = functions.https.onRequest((req, res) => {
+    const { toNumber, body } = req.body
+    admin.messaging().sendToPhoneNumber({
+        to: toNumber,
+        body,
+    })
+    res.json({
+        messageSid: "ok",
+    })
+})
+// exports.sendSMS = functions.https.onRequest((req, res) => {
+//     const { toNumber, body } = req.body
+//     // admin.messaging().sendToPhoneNumber({
+//     //     to: toNumber,
+//     //     body,
+//     // })
+//     const from = "Vonage APIs"
+//     const to = toNumber
+//     const text = body
+
+//     async function sendSMSVonage() {
+//         await vonage.sms.send({ to, from, text })
+//             .then(resp => {
+//                 console.log('Message sent successfully')
+//                 console.log(resp)
+//             })
+//             .catch(err => {
+//                 console.log('There was an error sending the messages.')
+//                 console.error(err)
+//             })
+//     }
+//     sendSMSVonage()
+//     res.json({
+//         messageSid: "ok",
+//     })
+// })
 
 
-async function sendCommand( topic, cmd, args)
-{
+
+
+
+async function sendCommand(topic, cmd, args) {
     // cmd:'ring',
     // args: '{"nombre":"Pablo"}'
 
@@ -139,11 +180,11 @@ async function sendCommand( topic, cmd, args)
     // args:
     try {
         const payload = {
-            topic:topic,
-            data: {cmd}
-        };
+            topic: topic,
+            data: { cmd }
+        }
         if (args)
-            payload.data["args"]=args
+            payload.data["args"] = args
 
         await admin.messaging().send(payload)
         console.log('Msg sent => pl:' + JSON.stringify(payload))
@@ -152,13 +193,12 @@ async function sendCommand( topic, cmd, args)
         console.log('Error sending msg:', err)
     }
 }
-async function sendPos( lat,lng)
-{
+async function sendPos(lat, lng) {
     try {
         const payload = {
-            data: {lat:lat.toString(), lng:lng.toString()},
-            topic:'PAM'
-        };
+            data: { lat: lat.toString(), lng: lng.toString() },
+            topic: 'PAM'
+        }
         await admin.messaging().send(payload)
         console.log('Msg sent ok to topic => pl:' + JSON.stringify(payload))
     }
@@ -166,13 +206,12 @@ async function sendPos( lat,lng)
         console.log('Error sending msg:', err)
     }
 }
-async function fcmPush(target, noti)
-{
+async function fcmPush(target, noti) {
     try {
         const payload = {
             notification: noti,
             data: {}
-        };
+        }
         //admin.messaging().sendToDevice('cLedIiDzTKe_GUvCbn0_qN:APA91bGkEmS0zYUqUrTCN_1ZSkb2L5AIkhIFgKnuxCcSz54fy8KbLfSa57Cjfhw5kiEGbOR97GTA2QtOBCbW4jlV6ZAZ', payload)
         await admin.messaging().sendToTopic(target, payload)
         console.log('Msg sent ok to ' + target + ' => pl:' + JSON.stringify(payload))
@@ -181,10 +220,8 @@ async function fcmPush(target, noti)
         console.log('Error sending msg:', err)
     }
 }
-async function sendMailTo(mail, person)
-{
-    return new Promise((resolve, reject) =>
-    {
+async function sendMailTo(mail, person) {
+    return new Promise((resolve, reject) => {
         let transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
@@ -195,8 +232,7 @@ async function sendMailTo(mail, person)
             }
         })
 
-        cors(req, res, () =>
-        {
+        cors(req, res, () => {
             console.log('mailTo: ' + mail + ' => ' + person)
             const mailOptions = {
                 //from: 'IntegralMente <integralmenteespacioterapeutic@gmail.com>',
@@ -209,8 +245,7 @@ async function sendMailTo(mail, person)
                 <br />
                 <img src="https://firebasestorage.googleapis.com/v0/b/pp-integralmente.appspot.com/o/integralmenteET.png?alt=media&token=2fe6482e-e6ff-4197-b2cf-2642e55e72c0" />`
             }
-            transporter.sendMail(mailOptions, (erro, info) =>
-            {
+            transporter.sendMail(mailOptions, (erro, info) => {
                 if (erro) {
                     console.log('error sending email:', erro.toString())
                     return reject(erro.toString())
